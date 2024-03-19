886920d8e561a681639f1502943312657d03316b
nfeng037 <nfeng@academic.rrc.ca>
2024-03-19 00:29:06 -0500

    Change the method of define strong parameters

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -5,14 +5,14 @@ ActiveAdmin.register Category do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description
+  permit_params :name, :description
   #
   # or
   #
-  permit_params do
-    permitted = [:name, :description]
-    permitted << :other if params[:action] == 'create' && current_user.admin?
-    permitted
-  end
+  # permit_params do
+  #   permitted = [:name, :description]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
 
 end
--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,14 +5,14 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :price, :stock, :category_id, :active
+  permit_params :name, :description, :price, :stock, :category_id, :active
   #
   # or
   #
-  permit_params do
-    permitted = [:name, :description, :price, :stock, :category_id, :active]
-    permitted << :other if params[:action] == 'create' && current_user.admin?
-    permitted
-  end
+  # permit_params do
+  #   permitted = [:name, :description, :price, :stock, :category_id, :active]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
 
 end
