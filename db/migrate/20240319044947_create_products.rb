74edbcf4fbaac594ccb132b1c9e17efd99b1ebb2
nfeng037 <nfeng@academic.rrc.ca>
2024-03-18 23:50:02 -0500

    Generate Product Model

---
+++ db/migrate/20240319044947_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock
+      t.references :category, null: false, foreign_key: true
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
