c6e8a929aebe1f4dc76813a58212e83990604bea
nfeng037 <nfeng@academic.rrc.ca>
2024-03-23 22:58:53 -0500

    Fixed the problem of being unable to turn pages when displaying products by brand name, and completed the product details page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
       @category = Category.find(params[:category_id])
       @products = @category.products.page(params[:page]).per(12)
     elsif params[:name]
-      @products = Product.where('name LIKE ?', "%#{params[:name]}%").page(params[:name]).per(12)
+      @products = Product.where('name LIKE ?', "%#{params[:name]}%").page(params[:page]).per(12)
     elsif params[:active] == "true"
       @products = Product.where(active: true).page(params[:page]).per(12)
     else
