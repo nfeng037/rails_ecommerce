05a2941d96bb5d014af2f9f79e6c0fa25d2a7cd4
nfeng037 <nfeng@academic.rrc.ca>
2024-04-04 23:26:29 -0500

    Add Devise to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :orders
   has_many :addresses
   has_one :cart
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'products#index'
 
   get 'cart', to: 'cart#show'
