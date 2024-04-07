08a6bb74b7906620cf685eb5e3d936ee8a6c47f2
nfeng037 <nfeng@academic.rrc.ca>
2024-04-06 22:22:30 -0500

    Add authentication links to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, unless: -> { guest? }
-  validates :password, presence: true, length: { minimum: 6 }, unless: -> { guest? }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
